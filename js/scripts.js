540fc6c668c708d5544aa35c736f601e9f944462
Asia Wasilewska <asia_wasilewska@tlen.pl>
2017-04-21 22:05:47 +0200

    First upload

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// plik scripts.js
+
+var a = prompt("podstawa trójkąta a=");
+var h = prompt("wysokość trójkąta h=");
+
+var triangleArea = a*h/2;
+
+console.log("Pole trójkąta o podstawie a: " + a + " i wysokości h: " + h + " wynosi: " + triangleArea);
